9727ffdb5592605400d7cc143b2a66fe3dac871a
Iñigo <mci.m89@gmail.com>
2018-01-23 12:47:39 +0100

    Se ha añadido la página principal llamada WELCOME situada en ROUTES y en RESOURCES->VIEWS, su CSS así como las imagenes utilizadas se situan en PUBLIC.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,162 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+<!DOCTYPE html>
+<html lang="en">
+<head>
 
-        <title>Laravel</title>
+  <title>SITU</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <!-- BOOTSTRAP LINKS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+  <!-- CSS LINK CON NOMENCLATURA LARAVEL -->
+  <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}" />
 
-            .full-height {
-                height: 100vh;
-            }
+</head>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<body>
 
-            .position-ref {
-                position: relative;
-            }
+  <nav class="navbar navbar-inverse  navbar-fixed-top"  role="navigation" >
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
 
-            .content {
-                text-align: center;
-            }
 
-            .title {
-                font-size: 84px;
-            }
+        <!-- IMAGEN HOME CON ICONO DE LA UFV -->
+        <li class=" li_resize navbar-brand-logo active">
+            <a href="/" align="center" style="padding: 0 0 0 0 "> 
+             <img width="50px" src="{{ asset('images/icono.jpg') }}" >
+         </a>
+     </li>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+<!-- BOTON "ABOUT" -->
+     <li>
+        <a href="about">
+            <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> About
+        </a>
+    </li>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+  <!--   <li class="dropdown">
+      <a href="index.html" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> 
+        <span class="glyphicon glyphicon-education" aria-hidden="true"></span> USUARIOS <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">ADMINISTRADOR</a></li>
+          <li><a href="#">ALUMNO</a></li>
+          <li><a href="#">PROFESOR</a></li>
+          <li><a href="#">INVITADO</a></li>
+      </ul>
+  </li> -->
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+<!-- BOTON CONTACT -->
+  <li><a href="contact_us">
+      <span class="glyphicon glyphicon-user" aria-hidden="true"></span> ContactUs</a>
+  </li>
+
+</ul>
+
+<!-- BOTÓN DE LOGIN -->
+<ul class="nav navbar-nav navbar-right" style="margin-right: 1%;">
+   <li><a data-toggle="modal" data-target="#loginModal"><span class="glyphicon glyphicon-log-in"></span> login</a></li>
+</ul>
+
+
+</div>
+</div>
+</nav>
+
+<!-- VENTANA EMERGENTE DE LOGIN  -->
+<div id="loginModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal"> &times;</button>
+          <h4><span class="glyphicon glyphicon-user"></span>Login</h4>
+
+      </div>
+      <div class="modal-body">
+         <form class="form-inline">
+           <div class="form-group">
+             <label class="sr-only" for="email">Email</label><input type="text" class="form-control input-sm" placeholder="Email" id="email" name="email">
+         </div>
+         <div class="form-group">  
+
+             <label class="sr-only" for="password">Password</label>
+             <input type="password" class="form-control input-sm" placeholder="Password" id="password" name="password"></div>
+             <div class="checkbox">
+               <label>
+                 <input type="checkbox"> Remember me
+             </label>
+         </div>
+
+
+
+         <button type="submit" class="btn btn-info btn-xs">Sign in</button>
+         <button type="button" class="btn btn-default btn-xs" data-dismiss="modal">Cancel</button> 
+
+
+
+
+     </form>
+ </div>
+</div>
+</div>
+</div>
+<!-- FIN DE LA VENTANA EMERGENTE DE LOGIN -->
+
+<!-- FOOTER CON INFORMACIÓN Y REDES SOCIALES (COPIADO DE LA PÁGINA WEB DE LA UFV) -->
+<div id="footer" align="center">
+    <table>
+      <tbody>
+
+       <tr> 
+        <td class="foot_izdo">&nbsp;</td> 
+        <td class="foot_cent"> 
+          <p class="foot_datos"> Universidad Francisco de Vitoria • Ctra. Pozuelo-Majadahonda Km. 1.800 • 28223 Pozuelo de Alarcón (Madrid, España)
+            <br> 
+            Teléfono: (+34) 91.351.03.03 • Fax: (+34) 91.351.17.16 
+        </p> 
+
+        <!-- REDES SOCIALES -->
+        <div id="social"> 
+            <a href="https://www.facebook.com/UFVmadrid/" class="enlace_social" target="_blank" rel="nofollow">
+              <img src="images/social/enl_soc_facebook_20.png" alt="Facebook">
+          </a> 
+          <a href="https://twitter.com/#!/ufvmadrid" class="enlace_social" target="_blank" rel="nofollow">
+              <img src="images/social/enl_soc_twitter_20.png" alt="Twitter">
+          </a>
+          <a href="https://www.youtube.com/user/ufvmadrid" class="enlace_social" target="_blank" rel="nofollow">
+              <img src="images/social/enl_soc_youtube_20.png" alt="Youtube">
+          </a>
+          <a href="https://www.linkedin.com/school/1205600/" class="enlace_social" target="_blank" rel="nofollow">
+              <img src="images/social/enl_soc_linkedin_20.png" alt="Linkedin">
+          </a> 
+          <a href="https://www.instagram.com/ufvmadrid/" class="enlace_social" target="_blank" rel="nofollow">
+              <img src="images/social/enl_soc_instagram_20.png" alt="Instagram">
+          </a>
+          <br><br>
+      </div>
+      <!-- FIN REDES SOCIALES -->
+
+      <a href="http://www.ufv.es/aviso-legal">Política de Privacidad</a> 
+      / Sponsored by the
+      <a href="http://legionariesofchrist.org/" rel="nofollow">Legionaries of Christ</a> 
+      and 
+      <a href="http://regnumchristi.es/" rel="nofollow">Regnum Christi</a> 
+      Copyright 2013,
+      <a href="http://legionariesofchrist.org/" rel="nofollow">Legion of Christ</a>
+      . All rights reserved. 
+  </td>
+  <td class="foot_dcho">&nbsp;</td> 
+</tr>
+
+</tbody>
+</table>
+</div>
+<div>
+
+</body>
 </html>
